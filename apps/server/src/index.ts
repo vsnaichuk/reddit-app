bf7d7cee9a8d2a1757e9716591ab8bf606ed40e3
Snaichuk Volodymyr <s.volodymyr@apiko.com>
2022-05-23 09:26:11 +0300

    Add register and login resolvers

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,6 +7,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 const init = async () => {
   const orm = await MikroORM.init<PostgreSqlDriver>(mikroConfig);
@@ -17,7 +18,7 @@ const init = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({
---
+++ apps/server/src/resolvers/user.ts
@@ -0,0 +1,130 @@
+import argon2 from 'argon2';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from 'type-graphql';
+import { User } from '../entities/User';
+import { ApolloContextType } from '../types';
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() ctx: ApolloContextType,
+  ): Promise<UserResponse> {
+    // TODO: Try to use validation libs
+    if (options.username.length <= 5) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Length must be greater than 5',
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 7) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Length must be greater than 7',
+          },
+        ],
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = ctx.em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await ctx.em.persistAndFlush(user);
+    } catch (err) {
+      // username already exist
+      if (err.code === '23505') {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'username already taken',
+            },
+          ],
+        };
+      }
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() ctx: ApolloContextType,
+  ): Promise<UserResponse> {
+    const user = await ctx.em.findOne(User, {
+      username: options.username,
+    });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'That username doe not exist',
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(
+      user.password,
+      options.password,
+    );
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Incorrect password',
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
+  }
+}
